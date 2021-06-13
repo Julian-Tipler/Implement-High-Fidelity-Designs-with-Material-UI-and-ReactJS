709fc52aa70dc70138834632aaaa0f80edaad624
Julian <tipler.julian@gmail.com>
2021-06-13 15:57:53 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  ThemeProvider,
+} from "@material-ui/styles";
+import theme from './ui/Theme'
+import Header from "./ui/Header";
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+const arcBlue = "#0B72B9"
+const arcOrange = "#FFBA60";
+
+export default createMuiTheme({
+    palette: {
+        common: {
+            arcBlue: "#0B72B9",
+            arcOrange: "#FFBA60"
+        },
+        primary: {
+            main: ""
+        }
+    }
+
+})
